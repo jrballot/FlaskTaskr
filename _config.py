5fc7057f8fddd959c683e7b46043a0816ad61dc1
Júlio Rangel Ballot <jrballot@Julios-MacBook-Pro.local>
2016-01-31 17:34:53 -0200

    SQLAlchemy + Registro de usuários

--- _config.py
+++ _config.py
@@ -3,9 +3,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.sb'
-USERNAME = 'admin'
-PASSWORD = 'admin'
+# USERNAME = 'admin'
+# PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x1f\xd5W2V\xae\xad\xe3\xe5\xb4Z\xddX\x14\x14\xb8"\x8cx\x94\xb99\xfaG'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database URI
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+DATABASE_PATH
--- views.py
+++ views.py
@@ -1,17 +1,21 @@
 
 
 
-import sqlite3
-from functools import wraps
+# import sqlite3
 
+from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, session, url_for, g
-from forms import AddTaskForm
+from forms import AddTaskForm, RegisterForm, LoginForm
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('_config')
+db = SQLAlchemy(app)
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE_PATH'])
+from models import Task, User
+
+# def connect_db():
+#     return sqlite3.connect(app.config['DATABASE_PATH'])
 
 def login_required(test):
     @wraps(test)
@@ -34,6 +38,7 @@ def logout():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
@@ -48,17 +53,22 @@ def login():
 @app.route('/tasks/')
 @login_required
 def tasks():
-    g.db = connect_db()
+    ## Código antigo sem SQLAlchemy
+    # g.db = connect_db()
+    #
+    # # query para selecionar as atividades abertas
+    # cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=1')
+    # open_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    #
+    # # query para selecionar as atividades fechadas
+    # cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=0')
+    # closed_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    #
+    # g.db.close()
 
-    # query para selecionar as atividades abertas
-    cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=1')
-    open_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
-
-    # query para selecionar as atividades fechadas
-    cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=0')
-    closed_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
-
-    g.db.close()
+    ## Código com SQLAlchemy
+    open_tasks = db.session.query(Task).filter_by(status=1).order_by(Task.due_date.asc())
+    closed_tasks = db.session.query(Task).filter_by(status=0).order_by(Task.due_date.asc())
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
@@ -71,42 +81,82 @@ def tasks():
 @app.route('/add/',methods=['POST'])
 @login_required
 def new_task():
-    g.db = connect_db()
-    name = request.form['name']
-    date = request.form['due_date']
-    priority = request.form['priority']
+    ## Código sem SQLAlchemy
+    # g.db = connect_db()
+    # name = request.form['name']
+    # date = request.form['due_date']
+    # priority = request.form['priority']
+    #
+    # if not name or not date or not priority:
+    #     flash("All fields are required. Please try again.")
+    #     return redirect(url_for('tasks'))
+    # else:
+    #     g.db.execute('insert into tasks (name, due_date, priority, status) values (?,?,?,1)',[
+    #         request.form['name'],
+    #         request.form['due_date'],
+    #         request.form['priority']
+    #     ])
+    #     g.db.commit()
+    #     g.db.close()
+    #     flash('New entry was successfully posted. Thanks.')
+    #     return redirect(url_for('tasks'))
 
-    if not name or not date or not priority:
-        flash("All fields are required. Please try again.")
-        return redirect(url_for('tasks'))
-    else:
-        g.db.execute('insert into tasks (name, due_date, priority, status) values (?,?,?,1)',[
-            request.form['name'],
-            request.form['due_date'],
-            request.form['priority']
-        ])
-        g.db.commit()
-        g.db.close()
-        flash('New entry was successfully posted. Thanks.')
-        return redirect(url_for('tasks'))
+    ## Código com SQLAlchemy
+    form = AddTaskForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_task = Task(form.name.data, form.due_date.data, form.priority.data, '1')
+            db.session.add(new_task)
+            db.session.commit()
+            flash("New entry was successfully posted. Thanks.")
+    return redirect(url_for('tasks'))
 
 @app.route('/complete/<int:task_id>')
 @login_required
 def complete(task_id):
-    g.db = connect_db()
-    g.db.execute('update tasks set status=0 where task_id='+str(task_id))
-    g.db.commit()
-    g.db.close()
-    flash('The task was marked as complete')
-    return redirect(url_for('tasks'))
+    ## Código sem SQLAlchemy
+    # g.db = connect_db()
+    # g.db.execute('update tasks set status=0 where task_id='+str(task_id))
+    # g.db.commit()
+    # g.db.close()
+    # flash('The task was marked as complete')
+    # return redirect(url_for('tasks'))
 
+    ## Código com SQLAlchemy
+    new_id = task_id
+    db.session.query(Task).filter_by(task_id=new_id).update({"status":"0"})
+    db.session.commit()
+    flash("The task is complete. Nice.")
+    return redirect(url_for('tasks'))
 
 @app.route('/delete/<int:task_id>/')
 @login_required
 def delete_entry(task_id):
-    g.db = connect_db()
-    g.db.execute('delete from tasks where task_id='+str(task_id))
-    g.db.commit()
-    g.db.close()
+    ## Código sem SQLAlchemy
+    # g.db = connect_db()
+    # g.db.execute('delete from tasks where task_id='+str(task_id))
+    # g.db.commit()
+    # g.db.close()
+    ## Código com SQLAlchemy
+    new_id = task_id
+    db.session.query(Task).filter_by(task_id=new_id).delete()
+    db.session.commit()
     flash('The task was deleted')
     return redirect(url_for('tasks'))
+
+@app.route('/register/', methods=['GET','POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please login.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
